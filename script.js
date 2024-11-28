846df3bb109eed891ad6eafad4c73a2f6e50a284
Talion Elessar <cagnin.lima.2007@gmail.com>
2024-11-27 21:46:56 -0300

    V4.05

--- script.js
+++ script.js
@@ -78,7 +78,12 @@ async function init() {
 
 init();
 
-async function fetchTop10Pokemon() {
+async function fetchPokemonList() {
+
+  document.getElementById('listButton').onclick = null;
+
+  window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
+
   const pokemonListElement = document.getElementById ("pokemonList");
 
   for (let i = 1; i <= 1302; i++) {
@@ -108,7 +113,7 @@ async function fetchTop10Pokemon() {
         pokemonName.textContent = data.name.toUpperCase();
         
         const pokemonId = document.createElement("p");
-        pokemonId.textContent = `ID: ${data.id}`;
+        pokemonId.innerHTML = ` <button onclick="fetchPokemonId(${data.id})">ID: ${data.id}</button>`;
 
         //adiciona os elementos ao Card 
         pokemonCard.appendChild(pokemonImage);
@@ -192,6 +197,65 @@ async function fetchPokemon(currentID) {
   }
 
   return currentID;
+}
+
+async function fetchPokemonId(idVar, currentID) {
+
+  window.scrollTo({top: 0, behavior: 'smooth'});
+
+  currentID = idVar;
+
+  try {
+    const response = await fetch(`http://localhost:${PORT}/pokemon/${idVar}`);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching Pokemon: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    //! SETTING DATA IN HTML
+    document.getElementById("pokemonInfo").style.display = "block";
+
+    //! SETTING POKEMON NAME & ID
+    document.getElementById("pokemonTitle").textContent =
+      data.name.toUpperCase();
+    document.getElementById("pokemonId").textContent = data.id;
+
+    //! SETTING POKEMON IMAGE
+    document.getElementById("pokemonImage").src = data.image;
+    document.getElementById("pokemonImage").alt = `Image of ${data.name}`;
+    document.getElementById("pokemonImageBack").src = data.imageBack;
+    document.getElementById("pokemonImageBack").alt = `Image of ${data.name}`;
+
+    //! SETTING POKEMON IMAGE SHINY
+    document.getElementById("pokemonImageShiny").src = data.imageShiny;
+    document.getElementById("pokemonImageShiny").alt = `Image of ${data.name}`;
+    document.getElementById("pokemonImageShinyBack").src = data.imageShinyBack;
+    document.getElementById(
+      "pokemonImageShinyBack"
+    ).alt = `Image of ${data.name}`;
+
+    //! SETTING POKEMON STATS
+    document.getElementById("pokemonOrder").textContent = data.order;
+    document.getElementById("pokemonTypes").textContent = data.types;
+    document.getElementById("pokemonBaseExperience").textContent =
+      data.base_experience;
+    document.getElementById("pokemonHeight").textContent = data.height;
+    document.getElementById("pokemonWeight").textContent = data.weight;
+    document.getElementById("pokemonAbilities").textContent = data.abilities;
+    document.getElementById("pokemonStats").textContent = data.stats;
+    document.getElementById("pokemonHeldItems").textContent = data.held_items;
+    document.getElementById("pokemonMoves").textContent = data.moves;
+  } catch (error) {
+    console.error("Error fetching Pokemon:", error);
+    document.getElementById("errorMessage").textContent =
+      "Error fetching Pokemon. Please try again.";
+    document.getElementById("errorMessage").style.display = "block";
+  } finally {
+    document.getElementById("loadingMessage").style.display = "none";
+  }
 }
 
 async function nextPokemon(currentID) {
